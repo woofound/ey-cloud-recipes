c04db01cd116ce9a231d478a9a1dc9d26934e3fc
Tom von Schwerdtner <tomvons@gmail.com>
2012-10-16 14:49:24 -0400

    whitespace

--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 if ['solo', 'util'].include?(node[:instance_role])
-  
+
   execute "install resque gem" do
     command "gem install resque redis redis-namespace yajl-ruby -r"
     not_if { "gem list | grep resque" }
@@ -16,35 +16,35 @@ if ['solo', 'util'].include?(node[:instance_role])
       else 
         worker_count = 4
     end
-  
 
-    node[:applications].each do |app, data|
-      template "/etc/monit.d/resque_#{app}.monitrc" do 
+
+  node[:applications].each do |app, data|
+    template "/etc/monit.d/resque_#{app}.monitrc" do 
       owner 'root' 
       group 'root' 
       mode 0644 
       source "monitrc.conf.erb" 
-      variables({ 
-      :num_workers => worker_count,
-      :app_name => app, 
-      :rails_env => node[:environment][:framework_env] 
+      variables({
+        :num_workers => worker_count,
+        :app_name => app, 
+        :rails_env => node[:environment][:framework_env] 
       }) 
-      end
+    end
 
-      worker_count.times do |count|
-        template "/data/#{app}/shared/config/resque_#{count}.conf" do
+    worker_count.times do |count|
+      template "/data/#{app}/shared/config/resque_#{count}.conf" do
         owner node[:owner_name]
         group node[:owner_name]
         mode 0644
         source "resque_wildcard.conf.erb"
-        end
       end
+    end
 
     execute "ensure-resque-is-setup-with-monit" do 
       epic_fail true
-      command %Q{ 
-      monit reload 
-      } 
+      command %Q{
+        monit reload
+      }
     end
   end 
 end
