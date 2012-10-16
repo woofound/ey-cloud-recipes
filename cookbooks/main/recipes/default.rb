4b8f41ac4762d53cec0d7c0545905fbc16fff6a7
Tom von Schwerdtner <tomvons@gmail.com>
2012-10-16 14:51:12 -0400

    the recipe handles the conditional checks, it seems...

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -47,9 +47,7 @@ require_recipe "resque"
 # require_recipe "resque-scheduler"
 
 #uncomment to run the redis recipe
-if node[:instance_role] == 'util'
-  require_recipe "redis"
-end
+require_recipe "redis"
 
 #require_recipe "logrotate"
 #
